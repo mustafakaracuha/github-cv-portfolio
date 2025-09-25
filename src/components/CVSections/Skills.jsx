9d72956465fc308bfe17743c985dfb926150c493
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-09-25 22:40:05 +0300

    edit skills

--- src/components/CVSections/Skills.jsx
+++ src/components/CVSections/Skills.jsx
@@ -6,63 +6,63 @@ const Skills = () => {
         <div className="skill-category">
           <h4 className="skill-category-title">Frontend</h4>
           <div className="skill-list">
-            <span className="skill-tag">React.JS</span>
-            <span className="skill-tag">Next.JS</span>
-            <span className="skill-tag">Vue.JS</span>
-            <span className="skill-tag">Angular.JS</span>
-            <span className="skill-tag">JavaScript</span>
-            <span className="skill-tag">TypeScript</span>
+            <span className="skill-tag react">React.JS</span>
+            <span className="skill-tag nextjs">Next.JS</span>
+            <span className="skill-tag vue">Vue.JS</span>
+            <span className="skill-tag angular">Angular.JS</span>
+            <span className="skill-tag javascript">JavaScript</span>
+            <span className="skill-tag typescript">TypeScript</span>
           </div>
         </div>
         
         <div className="skill-category">
           <h4 className="skill-category-title">State Yönetimi</h4>
           <div className="skill-list">
-            <span className="skill-tag">Redux Toolkit</span>
-            <span className="skill-tag">Zustand</span>
-            <span className="skill-tag">Vuex</span>
+            <span className="skill-tag redux">Redux Toolkit</span>
+            <span className="skill-tag zustand">Zustand</span>
+            <span className="skill-tag vuex">Vuex</span>
           </div>
         </div>
         
         <div className="skill-category">
           <h4 className="skill-category-title">UI Tasarımı</h4>
           <div className="skill-list">
-            <span className="skill-tag">CSS</span>
-            <span className="skill-tag">Tailwind CSS</span>
-            <span className="skill-tag">ShadCN</span>
-            <span className="skill-tag">Material UI</span>
-            <span className="skill-tag">Daisy UI</span>
-            <span className="skill-tag">Headless UI</span>
-            <span className="skill-tag">AntDesign</span>
+            <span className="skill-tag css">CSS</span>
+            <span className="skill-tag tailwind">Tailwind CSS</span>
+            <span className="skill-tag shadcn">ShadCN</span>
+            <span className="skill-tag material">Material UI</span>
+            <span className="skill-tag daisy">Daisy UI</span>
+            <span className="skill-tag headless">Headless UI</span>
+            <span className="skill-tag antd">AntDesign</span>
           </div>
         </div>
         
         <div className="skill-category">
           <h4 className="skill-category-title">API & Versiyon Kontrol</h4>
           <div className="skill-list">
-            <span className="skill-tag">RESTful API</span>
-            <span className="skill-tag">Git</span>
-            <span className="skill-tag">GitLab</span>
-            <span className="skill-tag">GitHub</span>
+            <span className="skill-tag api">RESTful API</span>
+            <span className="skill-tag git">Git</span>
+            <span className="skill-tag gitlab">GitLab</span>
+            <span className="skill-tag github">GitHub</span>
           </div>
         </div>
         
         <div className="skill-category">
           <h4 className="skill-category-title">Tasarım Araçları</h4>
           <div className="skill-list">
-            <span className="skill-tag">Figma</span>
-            <span className="skill-tag">Adobe Photoshop</span>
-            <span className="skill-tag">Adobe Illustrator</span>
+            <span className="skill-tag figma">Figma</span>
+            <span className="skill-tag photoshop">Adobe Photoshop</span>
+            <span className="skill-tag illustrator">Adobe Illustrator</span>
           </div>
         </div>
         
         <div className="skill-category">
           <h4 className="skill-category-title">Çalışma Yaklaşımı</h4>
           <div className="skill-list">
-            <span className="skill-tag">Agile yöntemler</span>
-            <span className="skill-tag">Temiz kod</span>
-            <span className="skill-tag">Takım çalışması</span>
-            <span className="skill-tag">Disiplin</span>
+            <span className="skill-tag agile">Agile yöntemler</span>
+            <span className="skill-tag clean">Temiz kod</span>
+            <span className="skill-tag team">Takım çalışması</span>
+            <span className="skill-tag discipline">Disiplin</span>
           </div>
         </div>
       </div>
